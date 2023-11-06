fe2a3a23ee47ef05a2a3a06fe5633ad8989bb9e6
SE-Team-C-2023 <chanvirakp@gmail.com>
2023-11-06 15:14:35 +0700

    Update

--- myapp/App.js
+++ myapp/App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState('');
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/data')
+      setData(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  };  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! this is me  </Text>
+      <Text>{data.message}</Text>
+      <Text>{data.name}</Text>
       <StatusBar style="auto" />
     </View>
   );
